013517f1a91bee7a823d0eacabc704109c63cfa6
izshivam <shivamkumarchoudhary8116@gmail.com>
2024-02-22 13:07:40 +0530

    conversion-operation & comparion completed

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,8 +1,39 @@
-"use strict"; // treat all JS code as newer version
+"use strict"; // treat a JS as newer version
 
-// alert( 3 + 3) // we are using nodejs, not browser
+// alert( 3 + 3) // Alert ko use krne ka diff. syntax hai Node ke andar, ye available hai, par esko avi use nhi karenge...... We are using node js, not browser
+
+// max. time when we type codes in React, nodejs , we can avoid semi column(;)
 
 console.log(3 
-    + 3)
+    +
+    3) // code readability should be high
 
-console.log("Shivam")
+    console.log("Shivam")
+
+
+let name = "Shivam"
+let age = 25
+let isLoggedIn = false   
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined =>
+// symbol => when is talk about uniqeness
+
+
+// Object
+
+console.log(typeof "Shivam");
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+console.log(typeof null);
+
+
+/** Undefined apne aap me ek type hai, But Null ek Object hai. **/
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,81 @@
+// let score = "33" 
+// let score = "33abc"
+// let score = null
+//  let score = undefined
+//  let score = true
+//  let score = "Shivam"
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+ /** "33" => 33
+    "33abc" => NaN
+    true => 1, false => 0 **/ 
+
+
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "Shivam"
+
+let booleanIsLoogedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoogedIn);
+
+/** 
+ 1 => true; 0 => false 
+ "" => false
+ "Shivam" => true
+**/
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************* Operation ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Shivam"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+console.log(num2);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+// Link to study
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
