c5cb0d9ec2ea2b9c6a916ce02707b70bf405d08d
izshivam <shivamkumarchoudhary8116@gmail.com>
2024-02-24 12:49:37 +0530

    datatypes completed

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,44 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 32658952326561n
+const bigNumber = 32658952326561n
+
+
+
+// Reference (Non Primitive)
+
+// Types : Arrays, Objects, Functions
+
+const heros = ["Shaktiman", "Naagraj", "Doga"]
+let myObj = {
+    name: "Shivam",
+    age: 25,
+}  
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
+console.log(typeof scoreValue);
+console.log(typeof myFunction);
+console.log(typeof heros);
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
